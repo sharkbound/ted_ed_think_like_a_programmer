a5da10e8a51ed01a7cd0ddaedd16bde4d85eaeb9
sharkbound <laptopblaster@gmail.com>
2019-10-23 22:28:50 -0500

    added resistance leader

---
+++ resistance_leader/find_the_resistance_leader.py
@@ -0,0 +1,61 @@
+import re
+from dataclasses import dataclass, field
+from enum import Flag, auto
+from random import choice
+from typing import Tuple
+
+
+def randbool():
+    return choice([True, False])
+
+
+class Flags(Flag):
+    none = 0
+    glasses = auto()
+    green_eyes = auto()
+    red_hair = auto()
+    no_glasses = auto()
+
+
+all_flags: Tuple[Flags] = tuple(Flags)
+vowels = 'aeiouy'
+re_consecutive_vowel = re.compile(r'(?P<vowel>[aeiouy])(?P=vowel)', flags=re.I)
+re_vowel = re.compile(r'[aeiouy]', flags=re.I)
+
+
+@dataclass
+class Person:
+    name: str
+    flags: Flags
+
+    def __and__(self, flags):
+        return self.flags & flags == flags
+
+
+def is_leader(person: Person):
+    if not person & Flags.green_eyes:
+        return False
+
+    bools = [True]
+
+    if person & Flags.glasses:
+        bools.append(len(re_vowel.findall(person.name)) == 2)
+    if person & Flags.red_hair:
+        bools.append(bool(re_consecutive_vowel.search(person.name)))
+    if person & Flags.no_glasses:
+        bools.append(len(re_consecutive_vowel.findall(person.name)) == 3)
+
+    return all(bools)
+
+
+people = [
+    Person('timmy', Flags.no_glasses),
+    Person('james', Flags.no_glasses),
+    Person('none', Flags.glasses | Flags.red_hair),
+    Person('just greaen', Flags.green_eyes | Flags.red_hair),
+    Person('vaal', Flags.glasses | Flags.green_eyes | Flags.red_hair),
+]
+
+for person in people:
+    if is_leader(person):
+        print(f'found leader "{person.name}" with flags "{person.flags}"')
---
+++ resistance_leader/notes.md
@@ -0,0 +1,11 @@
+# finding the leader
+
+## leader facial features
+* green eyes
+* red hair, name has 2 consecutive letters
+* glasses, name has exactly 2 vowels
+* no glasses, name has exactly 3 vowels
+
+## facts
+* Y is treated as a vowel
+* only one match when all are true
